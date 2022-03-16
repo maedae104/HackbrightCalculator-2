4e850e38fbec4e650ccc5545ad1fc3015eaa5d72
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-03-16 16:15:50 -0700

    added operator functions

--- arithmetic.py
+++ arithmetic.py
@@ -47,3 +47,13 @@ def mod(num1, num2):
     """Return the remainder of num / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Add num1 and num2 and multiply sum by num3."""
+
+    return multiply(add(num1, num2), num3)
+
+def add_cubes(num1, num2):
+    """Cube num1 and num2 and return the sum of these cubes."""
+
+    return add(cube(num1), cube(num2))
--- calculator.py
+++ calculator.py
@@ -2,6 +2,56 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
-    
+
+while True:
+    #take in user input indefinitely 
+    user_input = input("Enter your equation. > ")
+     #split/tokenize user input
+    tokens = user_input.split(" ")
+    operator = tokens[0]
+    result = None
+    num1 = tokens[1]
+    num2 = tokens[2]
+    num3 = tokens[3]
+    #unless "q" inputted
+    if user_input == "q" or "quit":
+        break
+
+    elif len(tokens) < 2:
+        print("Not enough inputs.")
+        continue
+
+    elif operator == "+":
+        add(num1, num2)
+
+    elif operator == "-":
+        subtract(num1, num2)
+
+    elif operator == "*":
+        multiply(num1, num2)
+
+    elif operator == "/":
+        divide(num1, num2)
+
+    elif operator == "%":
+        mod(num1, num2)
+
+    elif operator == "**":
+        power(num1, num2)
+
+    elif operator == "square":
+        square(num1)
+
+    elif operator == "cube":
+        cube(num1)
+
+    elif operator == "add_mult":
+        add_mult(num1, num2)
+
+    elif operator == "%"
+        add(num1, num2)
+   
+    #based on user input calculate run math function
+
 
 # Replace this with your code
