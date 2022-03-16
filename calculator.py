fc6a1efcb4213f0bc2bd98bf3b46b33765d292ed
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-03-16 15:42:57 -0700

    first commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+# Replace this with your code
